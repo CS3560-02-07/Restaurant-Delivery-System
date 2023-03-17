08ffd4e9e2eea21cb953e4e542fa4d766bc56cd2
Melvin <melvingit@gmail.com>
2023-03-16 23:35:16 -0700

    start delivery.java

---
+++ Delivery.java
@@ -0,0 +1,8 @@
+public class Delivery{
+    private int deliveryNumber;
+    private double estimatedDeliveryTime;
+    private double timestamp;
+    private double distance;
+
+    public Delivery(int deliveryNumber, double )
+}
