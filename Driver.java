62e68898a0c26587f6498b668b7218158b0d6d11
carlosro0505 <59211278+carlosro0505@users.noreply.github.com>
2023-03-16 22:54:17 -0700

    Update Driver.java
    comments

--- Driver.java
+++ Driver.java
@@ -1,11 +1,11 @@
 public class Driver{
-    private int driverID;
+    private int driverID;  
     private String name;
     private String carInfo; // "Year Make Model" 
     private String driversLicense;
     private boolean history; // Clean driving history?
 
-    public Driver(int driverID, String name, String carInfo, String driversLicense, boolean history){
+    public Driver(int driverID, String name, String carInfo, String driversLicense, boolean history){   
         this.driverID = driverID;
         this.name = name;
         this.carInfo = carInfo;
@@ -13,35 +13,35 @@ public class Driver{
         this.history = history;
     }
 
-    public void setDriverID(int newDriverID){
+    public void setDriverID(int newDriverID){   //sets the driver's ID number
         driverID = newDriverID;
     }
 
-    public void setName(String newName){
+    public void setName(String newName){    //sets the driver's name
         name = newName;
     }
 
-    public void setCarInfo(String newCarInfo){
+    public void setCarInfo(String newCarInfo){  //sets the driver's car info
         carInfo = newCarInfo;
     }
 
-    public void setDriversLicense(String newDriversLicense){
+    public void setDriversLicense(String newDriversLicense){ //sets the driver's license info
         driversLicense = newDriversLicense;
     }
 
-    public int getDriverID(){
+    public int getDriverID(){   //gets the driver's ID number
         return driverID;
     }
 
-    public String getName(){
+    public String getName(){    //gets the driver's name
         return name;
     }
 
-    public String getCarInfo(){
+    public String getCarInfo(){ //gets the driver's car info
         return carInfo;
     }
 
-    public String getDriversLicense(){
+    public String getDriversLicense(){  //gets the driver's license info
         return driversLicense;
     }
 
