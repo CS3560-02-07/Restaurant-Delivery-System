921da01154adf70b31bb2ed48de636946e68235f
Melvin <melvingit@gmail.com>
2023-03-17 00:02:37 -0700

    finish getters/setteres

--- Delivery.java
+++ Delivery.java
@@ -1,8 +1,50 @@
 public class Delivery{
-    private int deliveryNumber;
-    private double estimatedDeliveryTime;
-    private double timestamp;
-    private double distance;
+    private int deliveryNumber; //key
+    private double estimatedDeliveryTime; //estimated delivery time calculated by system
+    private double timestamp; //how long the delivery actually took
+    private double distance; //
 
-    public Delivery(int deliveryNumber, double )
+    public Delivery(int deliveryNum, double estimate, double time, double dist){
+        //Set all inputted information when a new instance is created
+        this.deliveryNumber = deliveryNum;
+        this.estimatedDeliveryTime = estimate;
+        this.timestamp = time;
+        this.distance = dist;
+
+    }
+
+    //Getters
+    public int getDeliveryNumber(){
+        return deliveryNumber;
+    }
+
+    public double getEstimatedDeliveryTime(){
+        return estimatedDeliveryTime;
+    }
+
+    public double getTimestamp(){
+        return timestamp;
+    }
+
+    public double getDistance(){
+        return distance;
+    }
+
+    //Setters
+    public void setDeliveryNumber(int newNum){
+        deliveryNumber = newNum;
+    }
+
+    public void setEstimatedDeliveryTime(double newEst){
+        estimatedDeliveryTime = newEst;
+    }
+
+    public void setTimestamp(double newTime){
+        timestamp = newTime;
+    }
+
+    public void setDistance(double newDist){
+        distance = newDist;
+    }
+
 }
