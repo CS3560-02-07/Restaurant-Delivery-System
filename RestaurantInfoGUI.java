7ecc662315169553973783e7de21e0184311e1ab
ayly8 <allison.y.ly@gmail.com>
2023-04-28 11:34:37 -0700

    updated restaurant ui

--- RestaurantInfoGUI.java
+++ RestaurantInfoGUI.java
@@ -13,11 +13,23 @@ public class RestaurantInfoGUI extends javax.swing.JFrame {
         //create tabs
         restInfoTab = new javax.swing.JTabbedPane();
         restOptionTabs = new javax.swing.JTabbedPane();
+        //restaurant view info tab + table
         viewInfo = new javax.swing.JPanel();
+        restViewInfoScrollPane = new javax.swing.JScrollPane();
+        restViewInfoTable = new javax.swing.JTable();
+        //request delivery labels (text on screen), text boxes, and button
         reqDelivery = new javax.swing.JPanel();
+        custOrderNumPrompt = new javax.swing.JLabel();
+        timeDelReqPrompt = new javax.swing.JLabel();
+        driverIDPrompt = new javax.swing.JLabel();
+        custOrderNumInput = new javax.swing.JTextField();
+        timeDelReqInput = new javax.swing.JTextField();
+        driverIDInput = new javax.swing.JTextField();
+        reqDelButton = new javax.swing.JButton();
+        //logout tab
         logOut = new javax.swing.JPanel();
         logOutPrompt = new javax.swing.JLabel();
-        //log out button for restaurant
+        //logout button for restaurant
         logOutButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE); //close window application
@@ -27,32 +39,89 @@ public class RestaurantInfoGUI extends javax.swing.JFrame {
 
         viewInfo.setBackground(new java.awt.Color(199, 234, 245)); //set background color for view info tab
 
+        //adds JTable
+        restViewInfoTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"JJ Boba", "3560 West Temple St. Pomona, CA", "9098968318"},
+                {null, null, null}
+            },
+            new String [] {
+                "Restaurant Name", "Address", "Phone Number"
+            }
+        ));
+        restViewInfoScrollPane.setViewportView(restViewInfoTable); 
+
         //horizontal and vertical layouts of view info tab
         javax.swing.GroupLayout viewInfoLayout = new javax.swing.GroupLayout(viewInfo);
         viewInfo.setLayout(viewInfoLayout);
         viewInfoLayout.setHorizontalGroup(
             viewInfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 641, Short.MAX_VALUE)
+            .addGroup(viewInfoLayout.createSequentialGroup()
+                .addGap(94, 94, 94)
+                .addComponent(restViewInfoScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(97, Short.MAX_VALUE))
         );
         viewInfoLayout.setVerticalGroup(
             viewInfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 317, Short.MAX_VALUE)
+            .addGroup(viewInfoLayout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addComponent(restViewInfoScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(40, Short.MAX_VALUE))
         );
 
         restOptionTabs.addTab("View Info", viewInfo); //names the tab "view info"
 
         reqDelivery.setBackground(new java.awt.Color(199, 234, 245)); //set background color of request delivery tab
 
+        //set font style and size for labels on request delivery tab
+        custOrderNumPrompt.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        custOrderNumPrompt.setText("Customer Order Number:");
+
+        timeDelReqPrompt.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        timeDelReqPrompt.setText("Time of Delivery Request:");
+
+        driverIDPrompt.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        driverIDPrompt.setText("Driver ID:");
+
+        reqDelButton.setText("Request Delivery");
+
         //horizontal and vertical layouts of request delivery tab
         javax.swing.GroupLayout reqDeliveryLayout = new javax.swing.GroupLayout(reqDelivery);
         reqDelivery.setLayout(reqDeliveryLayout);
         reqDeliveryLayout.setHorizontalGroup(
             reqDeliveryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 641, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, reqDeliveryLayout.createSequentialGroup()
+                .addContainerGap(132, Short.MAX_VALUE)
+                .addGroup(reqDeliveryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(custOrderNumPrompt, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(timeDelReqPrompt, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(driverIDPrompt, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(reqDeliveryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(reqDelButton, javax.swing.GroupLayout.DEFAULT_SIZE, 138, Short.MAX_VALUE)
+                    .addComponent(custOrderNumInput)
+                    .addComponent(timeDelReqInput)
+                    .addComponent(driverIDInput))
+                .addGap(204, 204, 204))
         );
         reqDeliveryLayout.setVerticalGroup(
             reqDeliveryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 317, Short.MAX_VALUE)
+            .addGroup(reqDeliveryLayout.createSequentialGroup()
+                .addGap(80, 80, 80)
+                .addGroup(reqDeliveryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(custOrderNumInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(custOrderNumPrompt))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(reqDeliveryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(timeDelReqInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(timeDelReqPrompt))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(reqDeliveryLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(driverIDInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(driverIDPrompt))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(reqDelButton)
+                .addContainerGap(121, Short.MAX_VALUE))
         );
 
         restOptionTabs.addTab("Request Delivery", reqDelivery); //names the tab "request delivery"
@@ -138,12 +207,21 @@ public class RestaurantInfoGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify                     
+    private javax.swing.JTextField custOrderNumInput;
+    private javax.swing.JLabel custOrderNumPrompt;
+    private javax.swing.JTextField driverIDInput;
+    private javax.swing.JLabel driverIDPrompt;
     private javax.swing.JPanel logOut;
     private javax.swing.JButton logOutButton;
     private javax.swing.JLabel logOutPrompt;
+    private javax.swing.JButton reqDelButton;
     private javax.swing.JPanel reqDelivery;
     private javax.swing.JTabbedPane restInfoTab;
     private javax.swing.JTabbedPane restOptionTabs;
+    private javax.swing.JScrollPane restViewInfoScrollPane;
+    private javax.swing.JTable restViewInfoTable;
+    private javax.swing.JTextField timeDelReqInput;
+    private javax.swing.JLabel timeDelReqPrompt;
     private javax.swing.JPanel viewInfo;
     // End of variables declaration                   
 }
