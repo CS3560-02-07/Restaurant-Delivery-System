d0baa5d6d62a987c85efedd5bcb00437289ce0d0
carlosro0505 <carlosro0505@gmail.com>
2023-05-09 16:45:49 -0700

    resolve

--- connectDatabase.java
+++ connectDatabase.java
@@ -350,21 +350,52 @@ public class connectDatabase {
         return results;
     }
 
-    public static String[] getRestOrders(int restID){
+    public static String[][] getRestOrders(int restID){
         try{
             Connection conn = getConnection();
             Statement state = conn.createStatement();
             String[] tableInfo;
-            ResultSet results = state.executeQuery("SELECT customerID, confirmed FROM orders WHERE restaurantID = " + String.valueOf(restID));
+
+            ResultSet results = state.executeQuery("SELECT customerID FROM orders WHERE restaurantID = " + String.valueOf(restID) + " AND confirmed = 0");
+            ResultSetMetaData info = results.getMetaData();
+            int columnNum = info.getColumnCount();
+
+            List<Integer> custIDs = new ArrayList<Integer>();
+            while (results.next()){
+                custIDs.add(results.getInt(1));
+            }
+
+            if (custIDs.size() == 0){
+                return new String[][] {{""}};
+            }
+
+            String answer[][] = new String[custIDs.size()][6];
+
+            results = state.executeQuery("SELECT order_num, total_cost FROM orders WHERE restaurantID = " + String.valueOf(restID) + " AND confirmed = 0");
+
+            int j = 0;
+            while (results.next()){
+                answer[j][0] = String.valueOf(results.getInt(1));
+                answer[j][4] = String.valueOf(results.getDouble(2));
+                answer[j][5] = String.valueOf(results.getDouble(2));
+                j++;
+            }
+
+            for (int i = 0; i < custIDs.size(); i++){
+                results = state.executeQuery("SELECT f_name, l_name, address, phone_number FROM customer WHERE customerID = " + String.valueOf(custIDs.get(i)));
                 
                 while (results.next()){
                     answer[i][1] = results.getString(1);
                     answer[i][2] = results.getString(2);
                     answer[i][3] = results.getString(3);
+                    answer[i][5] = results.getString(4);
                     answer[i][4] = results.getString(4);
 
                 }
-             results = state.executeQuery("SELECT f_name, l_name, address, phone_number FROM customer WHERE order")
+            }
+            state.close();
+            conn.close();
+            return answer;
         }
         catch(Exception e){
             throw new IllegalStateException("Failed to connect. ", e);
